c8f9a93e3b35923e7eda97085f19eff668495fe6
DontPanic494 <dontpanic494@gmail.com>
2017-11-13 23:54:52 -0500

    commend the samplemodel

--- App/Models/SampleModel.php
+++ App/Models/SampleModel.php
@@ -6,23 +6,24 @@ use PDO;
 use Core\Model;
 
 /**
-* sample model class
-*/
+ *
+ * Model SampleModel
+ * model for the "sample" data
+ *
+ */
+
 class SampleModel extends Model
 {
   
   public static function getAll() {
 
     try {
-      
+
       $db = static::getDB();
-
       $query = $db->query('select * from sample');
-
       $results = $query->fetchAll(PDO::FETCH_ASSOC);
 
       return $results;
-      
     } catch (PDOException $e) {
       echo 'Connection failed: ' . $e->getMessage();
     }
