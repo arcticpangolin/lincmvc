72ec04482fc49a6526a0c8b3f71f8214045c6bff
DontPanic494 <dontpanic494@gmail.com>
2017-11-14 01:16:24 -0500

    comment the home controller

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -1,12 +1,13 @@
 <?php
 
 /**
-
-  TODO:
-  - So many notes
-  - Just make this actually a real thing not just  ahacky test.
-
+ *
+ * Controller HomeController
+ * example controller displaying what ships with the framework
+ *
  */
+
+
 namespace App\Controllers;
 use Core\Controller;
 use Core\View;
@@ -15,19 +16,42 @@ use App\Models\SampleModel;
 class HomeController extends Controller
 {
   
+  /**
+   *
+   * Function indexAction
+   * renders the index template
+   *
+   * @return void
+   */
+
+  public function indexAction() {
+    $samples = SampleModel::getAll();
+    View::renderTemplate('Home/index.twig', ['samples' => $samples]);
+  }
+
+  /**
+   *
+   * Function before - action filter
+   * called before the execution of the controller action
+   * 
+   * @return void
+   */
 
   protected function before() {
     # code to run before action
     # return false to not execute action
   }
 
+  /**
+   *
+   * Function after - action filter
+   * called after the execution of the controller action
+   * 
+   * @return void
+   */
+
   protected function after() {
     # code to run after action
-  }
-
-  public function indexAction() {
-    $samples = SampleModel::getAll();
-    View::renderTemplate('Home/index.twig', ['samples' => $samples]);
   }
 
 }
