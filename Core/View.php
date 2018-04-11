842159e78579320227ce3f16c16997e9847d382c
Jay <jlincoln88@gmail.com>
2018-04-10 23:44:22 -0400

    started to clean up Core files

--- Core/View.php
+++ Core/View.php
@@ -11,45 +11,40 @@ namespace Core;
 
 class View
 {
-  /**
-   * 
-   * Function render
-   * render a view
-   *
-   * @param string $view - the view file
-   * @return void
-   */
+    /**
+     * Renders a view.
+     *
+     * @param string $view - the view file
+     * @return void
+     */
+    public static function render($view, $args = [])
+    {
+        extract($args, EXTR_SKIP);
 
-  public static function render($view, $args = []) {
-    extract($args, EXTR_SKIP);
+        $file = "../App/Views/$view";
 
-    $file = "../App/Views/$view";
-
-    if (is_readable($file)) {
-      require $file;
-    } else {
-      throw new \Exception("$file not found");
-    }
-  }
-
-  /**
-   * 
-   * Function renderTemplate
-   * render a twig template
-   *
-   * @param string $template - the template file
-   * @return void
-   */
-
-  public static function renderTemplate($template, $args = []) {
-    static $twig = null;
-
-    if ($twig === null) {
-      $loader = new \Twig_Loader_Filesystem('../App/Views');
-      $twig = new \Twig_Environment($loader);
+        if (is_readable($file)) {
+            require $file;
+        } else {
+            throw new \Exception("$file not found");
+        }
     }
 
-    echo $twig->render($template, $args);
-  }
-  
+    /**
+     * Renders a twig template.
+     *
+     * @param string $template - the template file
+     * @return void
+     */
+    public static function renderTemplate($template, $args = [])
+    {
+        static $twig = null;
+
+        if ($twig === null) {
+            $loader = new \Twig_Loader_Filesystem('../App/Views');
+            $twig = new \Twig_Environment($loader);
+        }
+
+        echo $twig->render($template, $args);
+    }
 }
